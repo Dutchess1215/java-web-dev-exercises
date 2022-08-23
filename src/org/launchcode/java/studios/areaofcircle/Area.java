9e3441830ab5b4d185de6fb81c2c648453265ed9
Nathan Murray <namurray1215@gmail.com>
2022-08-22 20:20:44 -0500

    studio

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -6,27 +6,28 @@ import static java.lang.Double.*;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         Integer loop = 0;
-        String radiusSt;
         Double radius = 0.0;
+
         while(loop == 0 ) {
             System.out.println("If you enter a radius, I'll tell you an area of a circle: ");
-            radiusSt = input.nextLine();
-            if (radiusSt == ""){
-                System.out.println("that is nothing");
-            } else {
-                radius = parseDouble(radiusSt);
-//                System.out.println(".." + radius);
+
+            try{
+                radius = input.nextDouble();
+                if (radius < 0){
+                    System.err.println("no negatives");
+                } else if(radius>0){
+                    loop++;
+                }
+
+            } catch (Exception error) {
+            System.err.println("that's not a number");
+            loop++;
             }
-            if (radius < 0){
-                System.out.println("no negatives");
-            }
-//            if (radius.isNaN()){
-//                System.out.println("that is not a number");
-//            }
-            if(radius>0){
-                loop++;
-            }
+
+
+
 
         }
         System.out.println(Circle.getArea(radius));
